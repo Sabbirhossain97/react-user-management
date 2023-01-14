2033d8aeecf61e5e39d3fff763593af611265298
Sabbirhossain94 <sabbirshawon1994@hotmail.com>
2023-01-14 17:17:01 +0600

    pagination problem fixed

--- src/App.js
+++ src/App.js
@@ -155,106 +155,104 @@ function App() {
         </div>
         <div className="mt-8 flex flex-col w-3/2">
           <div className="-my-2 -mx-4 sm:mx-auto lg:mx-auto w-10/12  ">
-            <div className="inline-block min-w-full py-2 align-middle ">
-              <div className="overflow-x-auto">
-                <table className="min-w-full border-separate ">
-                  <thead className="bg-slate-300">
-                    <tr>
-                      <th className="sticky w-1/12 top-0 z-10 text-center border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 sm:table-cell">
-                        ID
-                      </th>
-                      <th className="sticky w-2/12 top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 px-3 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:table-cell">
-                        Name
-                      </th>
-                      <th className="sticky w-3/12 top-0 z-10  border-b border-gray-300 bg-gray-50 bg-opacity-75 pl-4 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell">
-                        Email
-                      </th>
-                      <th className="sticky w-2/12 top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:table-cell">
-                        Phone
-                      </th>
-                      <th className="sticky w-2/12 top-0 z-10  border-b border-gray-300 bg-gray-50 bg-opacity-75 pl-4 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell">
-                        Country
-                      </th>
+            <div className="inline-block min-w-full align-middle ">
+              <table className="min-w-full border-separate ">
+                <thead className="bg-slate-300">
+                  <tr>
+                    <th className="sticky w-1/12 top-0 z-10 text-center border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 sm:table-cell">
+                      ID
+                    </th>
+                    <th className="sticky w-2/12 top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 px-3 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:table-cell">
+                      Name
+                    </th>
+                    <th className="sticky w-3/12 top-0 z-10  border-b border-gray-300 bg-gray-50 bg-opacity-75 pl-4 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell">
+                      Email
+                    </th>
+                    <th className="sticky w-2/12 top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:table-cell">
+                      Phone
+                    </th>
+                    <th className="sticky w-2/12 top-0 z-10  border-b border-gray-300 bg-gray-50 bg-opacity-75 pl-4 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell">
+                      Country
+                    </th>
 
-                      <th className="sticky top-0 z-10 border-b border-gray-300  bg-gray-50 bg-opacity-75 px-3 py-3.5 text-sm text-center font-semibold text-gray-900 backdrop-blur backdrop-filter">
-                        Action
-                      </th>
-                    </tr>
-                  </thead>
-                  <TransitionGroup component="tbody" className="bg-white">
-                    {users
-                      .filter(
-                        (item, key) =>
-                          key >= (currentPage - 1) * itemsPerPage &&
-                          key <= itemsPerPage * currentPage - 1
-                      )
-                      .map((user, key) => (
-                        <CSSTransition
-                          key={user.id}
-                          in={user.length >0 ? true: false }
-                          classNames="slide-vertical"
-                          timeout={300}
-                        >
-                          <tr>
-                            <td className="whitespace-nowrap border-b border-gray-200 pl-4 py-4 text-sm text-center font-medium text-gray-900">
-                              {user.id}
-                            </td>
-                            <td className="whitespace-nowrap border-b border-gray-200 px-3 py-4 pl-4 text-sm font-medium text-gray-900 lg:pl-6">
-                              {user.firstName} {user.lastName}
-                            </td>
-                            <td className="whitespace-nowrap border-b border-gray-200 px-3 py-4 pl-4 text-sm font-medium text-gray-900 hidden lg:table-cell">
-                              {user.email}
-                            </td>
-                            <td className="whitespace-nowrap border-b border-gray-200 px-3 py-4 pl-4 text-sm font-medium text-gray-900 hidden lg:pl-6 sm:table-cell">
-                              {user.phone}
-                            </td>
-                            <td className="whitespace-nowrap border-b border-gray-200 px-3 py-4 pl-4 text-sm font-medium text-gray-900 hidden sm:table-cell">
-                              {user.country}
-                            </td>
+                    <th className="sticky top-0 z-10 border-b border-gray-300  bg-gray-50 bg-opacity-75 px-3 py-3.5 text-sm text-center font-semibold text-gray-900 backdrop-blur backdrop-filter">
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+                <TransitionGroup component="tbody" className="bg-white">
+                  {users
+                    .filter(
+                      (item, key) =>
+                        key >= (currentPage - 1) * itemsPerPage &&
+                        key <= itemsPerPage * currentPage - 1
+                    )
+                    .map((user, key) => (
+                      <CSSTransition
+                        key={user.id}
+                        in={user.length > 0 ? true : false}
+                        classNames="slide"
+                        timeout={300}
+                      >
+                        <tr>
+                          <td className="whitespace-nowrap border-b border-gray-200 pl-4 text-sm text-center font-medium text-gray-900">
+                            {user.id}
+                          </td>
+                          <td className="whitespace-nowrap border-b border-gray-200 px-3 pl-4 text-sm font-medium text-gray-900 lg:pl-6">
+                            {user.firstName} {user.lastName}
+                          </td>
+                          <td className="whitespace-nowrap border-b border-gray-200 px-3 pl-4 text-sm font-medium text-gray-900 hidden lg:table-cell">
+                            {user.email}
+                          </td>
+                          <td className="whitespace-nowrap border-b border-gray-200 px-3 pl-4 text-sm font-medium text-gray-900 hidden lg:pl-6 sm:table-cell">
+                            {user.phone}
+                          </td>
+                          <td className="whitespace-nowrap border-b border-gray-200 px-3 pl-4 text-sm font-medium text-gray-900 hidden sm:table-cell">
+                            {user.country}
+                          </td>
 
-                            <td className="relative whitespace-nowrap border-b border-gray-200 py-4 pr-4 pl-3 text-right text-sm font-medium sm:pr-6 lg:pr-8">
-                              <div className="flex justify-center">
-                                <a
-                                  onClick={() => removeTodo(user.id)}
-                                  className="text-red-700 cursor-pointer hover:text-red-800"
+                          <td className="relative whitespace-nowrap border-b border-gray-200 py-4 pr-4 pl-3 text-right text-sm font-medium sm:pr-6 lg:pr-8">
+                            <div className="flex justify-center">
+                              <a
+                                onClick={() => removeTodo(user.id)}
+                                className="text-red-700 cursor-pointer hover:text-red-800"
+                              >
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  viewBox="0 0 24 24"
+                                  fill="currentColor"
+                                  className="w-5 h-5"
                                 >
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24"
-                                    fill="currentColor"
-                                    className="w-5 h-5"
-                                  >
-                                    <path
-                                      fillRule="evenodd"
-                                      d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
-                                      clipRule="evenodd"
-                                    />
-                                  </svg>
-                                </a>
-                                <a
-                                  onClick={() => {
-                                    editModal(user);
-                                  }}
-                                  className="text-indigo-600 cursor-pointer hover:text-indigo-900"
+                                  <path
+                                    fillRule="evenodd"
+                                    d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
+                                    clipRule="evenodd"
+                                  />
+                                </svg>
+                              </a>
+                              <a
+                                onClick={() => {
+                                  editModal(user);
+                                }}
+                                className="text-indigo-600 cursor-pointer hover:text-indigo-900"
+                              >
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  viewBox="0 0 24 24"
+                                  fill="currentColor"
+                                  className="w-5 h-5"
                                 >
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24"
-                                    fill="currentColor"
-                                    className="w-5 h-5"
-                                  >
-                                    <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
-                                    <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
-                                  </svg>
-                                </a>
-                              </div>
-                            </td>
-                          </tr>
-                        </CSSTransition>
-                      ))}
-                  </TransitionGroup>
-                </table>
-              </div>
+                                  <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
+                                  <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
+                                </svg>
+                              </a>
+                            </div>
+                          </td>
+                        </tr>
+                      </CSSTransition>
+                    ))}
+                </TransitionGroup>
+              </table>
             </div>
           </div>
         </div>
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -10,7 +10,7 @@ const Pagination = ({
 }) => {
   useEffect(() => {
     setTotalPage(Math.ceil(users.length / itemsPerPage));
-  }, [itemsPerPage,users.length]);
+  }, [itemsPerPage, users.length]);
 
   let paginationArray = [];
   for (let i = 1; i <= totalPage; i++) {
@@ -60,17 +60,19 @@ const Pagination = ({
             Previous
           </button>
           {paginationArray.map((item, key) => (
-            <div key={key} className="h-[4px]">
-              <p
-                className={`${
-                  currentPage === item
-                    ? "bg-blue-600 text-white px-4 py-2 text-center font-medium rounded-md cursor-pointer mr-2"
-                    : "px-4 py-2 font-medium text-blue-600 border border-gray-200 rounded-md cursor-pointer mr-2 hover:bg-blue-600 hover:text-white"
-                }`}
-              >
-                {item}
-              </p>
-            </div>
+            <p
+              key={key}
+              onClick={() => {
+                setCurrentPage(item);
+              }}
+              className={`${
+                currentPage === item
+                  ? "bg-blue-600 text-white px-4 py-2 text-center font-medium rounded-md cursor-pointer mr-2"
+                  : "px-4 py-2 font-medium text-blue-600 border border-gray-200 rounded-md cursor-pointer mr-2 hover:bg-blue-600 hover:text-white"
+              }`}
+            >
+              {item}
+            </p>
           ))}
           <button
             onClick={() => nextPage()}
